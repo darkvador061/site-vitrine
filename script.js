1754a8324cf327146e49c50e123606d419f48660
Walter <vignawalter@gmail.com>
2021-02-09 16:08:00 +0100

    modification responsive

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-//NAVBAR
-let lastScrollTop = 0;
-navbar = document.getElementById('navbar');
 
-window.addEventListener('scroll', function() {
-    const scrollTop = window.pageTOffset || this.document.documentElement.scrollTop;
-
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top="-50px";
-    }else{
-        navbar.style.top = "0"
-    }
-    lastScrollTop = scrollTop;
-});
 
 // TYPED
 
